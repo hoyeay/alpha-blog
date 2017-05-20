71af22bbdb37fc982f77a34c4fa6991c5b4be92b
JOSE RAMIREZ <hoyeay5@gmail.com>
2017-05-20 06:46:07 -0500

    Create new template, action, and route

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
+  # Validates article titles and descriptions
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
   validates :description, presence: true, length: { minimum: 10, maximum: 800 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # add articles table to resources
+  resources :articles
+  # set main web page
   root 'pages#home'
+  # set about page
   get 'about', to: 'pages#about'
 end
