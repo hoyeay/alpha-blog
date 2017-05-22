eb6a58fe56f0c467f459f4a27a22a273bafa88d3
JOSE RAMIREZ <hoyeay5@gmail.com>
2017-05-21 22:16:27 -0500

    Added info to lines

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,41 +1,41 @@
 # articles controller
 class ArticlesController < ApplicationController
-  # apply set_article from private to these methods ONLY
+  # apply set_article from private, below,  to these methods ONLY
   before_action :set_article, only: [:edit, :update, :show, :destroy]
 
-  # display all articles
+  # display all articles in the Article DB
   def index
     @articles = Article.all
   end
 
   # create a new article for the Article DB
+  # @article is the instance variable for the new Article db
   def new
     @article = Article.new
   end
 
   # edit an article
-  def edit
+  def edit # displays the edit.html.slim page with article form
   end
 
   # update the article and save
   def update
-    set_article
-    if @article.update(article_params)
-      flash[:success] = 'Article was successfully updated!'
-      redirect_to article_path(@article)
+    if @article.update(article_params) # if article is updated
+      flash[:success] = 'Article was successfully updated!' # flash message
+      redirect_to article_path(@article) # redirect to article
     else
-      render 'edit'
+      render 'edit' # display the edit method/view (edit.html.slim)
     end
   end
 
   # create the article and save it to the Article DB
   def create
     @article = Article.new(article_params)
-    if @article.save
-      flash[:success] = 'Article was successfully created!'
-      redirect_to article_path(@article)
+    if @article.save # if article is saved
+      flash[:success] = 'Article was successfully created!' # display flash message
+      redirect_to article_path(@article) # redirect to created article
     else
-      render 'new'
+      render 'new' # display the new method/view (new.html.slim)
     end
   end
 
@@ -43,18 +43,20 @@ class ArticlesController < ApplicationController
   def show
   end
 
-  # find and destroy article
+  # delete the article from Article DB
   def destroy
-    @article.destroy
-    flash[:danger] = 'Article was successfully deleted. :('
-    redirect_to articles_path
+    @article.destroy # destroy article
+    flash[:danger] = 'Article was successfully deleted.' # flash message
+    redirect_to articles_path # redirect to all articles
   end
 
+  # private methods can only be accessed within this class
   private
+  # finds the current article id in the Article Db
   def set_article
-    @article = Article.find(params[:id])
+    @article = Article.find(params[:id]) # obtain the currently Article's id from Article DB
   end
-
+  # permit the title and description params onto the Article DB
   def article_params
     params.require(:article).permit(:title, :description)
   end
