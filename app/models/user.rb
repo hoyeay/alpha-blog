816db5bbbbd9d11b6ab5c49d787ef784b505377f
JOSE RAMIREZ <hoyeay5@gmail.com>
2017-05-23 03:19:50 -0500

    Add association to user and articles and add debug under footer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ActiveRecord::Base
+  has_many :articles
+  # lowcase email before saving to User DB
+  before_save { self.email = email.downcase }
+  # validates username
   validates :username, presence: true, 
             uniqueness: { case_sensitive: false }, 
             length: { minumum: 1, maximum: 25 }
+  # validates email
   VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, 
             length: { maximum: 105 }, 
