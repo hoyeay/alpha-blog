713d40823f2cd1cdb8205b4e47ac3d262a32f2b3
JOSE RAMIREZ <hoyeay5@gmail.com>
2017-05-23 02:53:59 -0500

    Added user validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
   # Validates article titles and descriptions
-  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
-  validates :description, presence: true, length: { minimum: 10, maximum: 800 }
+  validates :title, presence: true, 
+            length: { minimum: 3, maximum: 50 }
+  validates :description, presence: true, 
+            length: { minimum: 10, maximum: 1500 }
 end
