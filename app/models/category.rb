89b8a89a2e50403fc7c80d53392eb327460ccc7b
JOSE RAMIREZ <hoyeay5@gmail.com>
2017-05-30 15:23:46 -0500

    added listing tests

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ActiveRecord::Base
   
   validates :name, 
             presence: true, 
-            length: { minimum: 3, maximum: 25 }
+            length: { minimum: 3, maximum: 20 }
   
   validates_uniqueness_of :name
 end
