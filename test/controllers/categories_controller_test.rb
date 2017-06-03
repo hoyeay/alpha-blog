22df530528e654fdf7a6ce17c57a27b8f66450ae
JOSE RAMIREZ <hoyeay5@gmail.com>
2017-06-03 00:46:56 -0500

    added admin requirement for categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -4,6 +4,10 @@ class CategoriesControllerTest < ActionController::TestCase
   
   def setup
     @category = Category.create(name: 'sports')
+    @user = User.create(username: 'john', 
+                        email: 'john@example.com', 
+                        password: 'password', 
+                        admin: true)
   end
   
   test 'should get categories index' do
@@ -12,6 +16,8 @@ class CategoriesControllerTest < ActionController::TestCase
   end
   
   test 'should get new' do
+    # simulate logged in user
+    session[:user_id] = @user.id
     get :new
     assert_response :success
   end
@@ -19,5 +25,12 @@ class CategoriesControllerTest < ActionController::TestCase
   test 'should get show' do
     get :show, params: { id: @category.id }
     assert_response :success
+  end
+  
+  test 'should redirect create when admin not logged in' do
+    assert_no_difference 'Category.count' do
+      post :create, params: {category: {name: 'sports'} }
+    end
+    assert_redirected_to categories_path
   end
 end
--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -2,15 +2,20 @@ require 'test_helper'
 
 class ListCategoriesTest < ActionDispatch::IntegrationTest
   
+  # set variables to use for testing
   def setup
     @category = Category.create(name: 'sports')
     @category2 = Category.create(name: 'programming')
   end
   
   test 'should show categories listing' do
+    # get categories_path page
     get categories_path
+    # test that the view for the method index in categories_controller.rb is displayed
     assert_template 'categories/index'
+    # test that @category exists as a path in category_path page
     assert_select 'a[href=?]', category_path(@category), text: @category.name
+    # test that @category2 exists as a path in category_path page
     assert_select 'a[href=?]', category_path(@category2), text: @category2.name
   end
 end
